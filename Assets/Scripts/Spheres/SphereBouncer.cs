688fe703a57de671f3dcae30e19753af206ac871
Hollowriter <rodrigodiazklipphan@gmail.com>
2023-03-28 18:57:41 -0300

    Two types of spheres with two different behaviours

---
+++ Assets/Scripts/Spheres/SphereBouncer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereBouncer : Sphere
+{
+    [SerializeField] private int timeToJump;
+    [SerializeField] private Rigidbody rb;
+    private float _jumpTimer;
+
+    private void Awake()
+    {
+        _jumpTimer = 0;
+    }
+
+    protected override void SphereBehaviour()
+    {
+        _jumpTimer += Time.deltaTime;
+        if (_jumpTimer >= timeToJump) 
+        {
+            rb.AddForce(Vector3.up * sphereImpulse, ForceMode.Impulse);
+            _jumpTimer = 0;
+        }
+    }
+
+    private void Update()
+    {
+        SphereBehaviour();
+    }
+}
---
+++ Assets/Scripts/Spheres/SphereFollower.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereFollower : Sphere
+{
+    protected override void SphereBehaviour()
+    {
+        float totalSpeed = sphereImpulse * Time.deltaTime;
+        this.transform.position = Vector3.MoveTowards(this.transform.position, TankMovement.instance.transform.position, totalSpeed);
+    }
+
+    private void Update()
+    {
+        SphereBehaviour();
+    }
+}
