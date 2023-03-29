2e2437eac5fc6f1fef19d4518948271c2d0ff447
Hollowriter <rodrigodiazklipphan@gmail.com>
2023-03-29 01:02:24 -0300

    Quit button set

---
+++ Assets/Scripts/UI/MenuUI/UIQuitButton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIQuitButton : Singleton<UIQuitButton>
+{
+    private void Awake()
+    {
+        SingletonAwake();
+    }
+
+    public void Quit() 
+    {
+        Application.Quit();
+    }
+}
